bacf7a34d2c9ce068e6df484cca48f2dc1388841
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-05-01 09:56:19 +0200

    feat: Add navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddCarDialogComponent } from './components/add-car-dialog/add-car-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'car-inventory';
+
+  constructor(private _dialogRef: MatDialog){}
+
+  openDialog() {
+    this._dialogRef.open(AddCarDialogComponent)
+  }
 }
---
+++ src/app/components/add-car-dialog/add-car-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-add-car-dialog',
+  templateUrl: './add-car-dialog.component.html',
+  styleUrls: ['./add-car-dialog.component.scss']
+})
+export class AddCarDialogComponent {
+  
+}
